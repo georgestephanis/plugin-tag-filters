fbd917cb9edba9dc542bfcce23999024a000d0a4
George Stephanis <daljo628@gmail.com>
2025-08-05 12:59:46 -0400

    Add filtering to the `plugins_api` result to stash the found tags.
    And then output them dynamically in the list.

--- plugin-tag-filters.php
+++ plugin-tag-filters.php
@@ -22,11 +22,14 @@ namespace PluginTagFilters;
 function on_init() {
 	add_action( 'install_plugins_table_header', __NAMESPACE__ . '\install_plugins_table_header' );
 	add_action( 'load-plugin-install.php', __NAMESPACE__ . '\add_styles_to_admin' );
+	add_filter( 'plugins_api_result', __NAMESPACE__ . '\filter_plugins_api_result', 10, 3 );
 }
 add_action ( 'init', __NAMESPACE__ . '\on_init' );
 
 /**
  * Add in our additional styles to the wp-admin stylesheets.
+ *
+ * @return void
  */
 function add_styles_to_admin() {
 	$asset_file = include( plugin_dir_path( __FILE__ ) . 'build/index.asset.php');
@@ -49,17 +52,51 @@ function add_styles_to_admin() {
 }
 
 /**
+ * Store the data we care about from the plugins api result, for later reference.
+ *
+ * Ideally we would just access the data on WP_Plugin_Install_List_Table->items -- if it's exposed when we want it.
+ *
+ * @param object|WP_Error $res    Response object or WP_Error.
+ * @param string          $action The type of information being requested from the Plugin Installation API.
+ * @param object          $args   Plugin API arguments.
+ */
+function filter_plugins_api_result( $res, $action, $args ) {
+	$tags = array();
+
+	foreach ( $res->plugins as $plugin ) {
+		$slug = $plugin['slug'];
+		if ( $plugin['tags'] && is_array( $plugin['tags'] ) ) {
+			foreach ( $plugin['tags'] as $tag ) {
+				$tags[ $tag ][] = $slug;
+			}
+		} else {
+			$tags['untagged'][] = $slug;
+		}
+	}
+
+	ksort( $tags );
+
+	$GLOBALS['ptf_plugin_result_tags'] = $tags;
+
+	return $res;
+}
+
+/**
  * Add the filtering links to the DOM above the table.
  *
  * @return void
  */
 function install_plugins_table_header() {
+	global $ptf_plugin_result_tags;
 	?>
 	<ul class="plugin-table-tag-filters">
-		<li><a href="javascript:;">Tag One (32)</a></li>
-		<li><a href="javascript:;">Tag Two (23)</a></li>
-		<li><a href="javascript:;">Tag Three (14)</a></li>
-		<li><a href="javascript:;">Tag Four (6)</a></li>
+		<?php
+		foreach ( $ptf_plugin_result_tags as $tag => $plugin_slugs ) {
+			if ( ( 'untagged' === $tag ) || count( $plugin_slugs ) > 1 ) {
+				printf( '<li><a href="javascript:;">%1$s (%2$d)</a></li>', esc_html( $tag ), count( $plugin_slugs ) );
+			}
+		}
+		?>
 	</ul>
 	<?php
 }
